60835284dfb2366d7c8897fb032f0e752f06ce89
Matt Lafferty <matt.tech.laff@gmail.com>
2026-03-01 13:26:43 -0800

    navbar styling cleaned up

--- js/navbar.js
+++ js/navbar.js
@@ -2,20 +2,24 @@
 const themes = ['t1', 't2', 't3', 't4', 't5', 't6', 't7', 't8', 't9'];
 let theme_idx = 0;
 const menuButton = document.getElementById('menu-toggle');
+const blurDiv = document.querySelector('.blur')
 
-document.getElementById('toggle').addEventListener('click', function() {
+document.getElementById('theme-toggle').addEventListener('click', function() {
     theme_idx = (theme_idx + 1) % themes.length;
     document.documentElement.dataset.theme = themes[theme_idx];
+    document.body.classList.toggle('dark-mode')
     // button.textContent = "Theme: " + themes[theme_idx];
 
 }); 
 
 menuButton.addEventListener('click', function() {
-    const navLinks = document.querySelector('.nav-links');
-    const navbar = document.querySelector('.navbar');
-    const blur = document.querySelector('.blur');
-    navLinks.classList.toggle('active');
-    navbar.classList.toggle('menu-open');
-    blur.classList.toggle('active');
+
+    document.body.classList.toggle("menu-open")
 
 });
+
+blurDiv.addEventListener('click', () => {
+    document.body.classList.toggle("menu-open")
+}
+)
+
